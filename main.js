a468ffd5a3ae520c0e257987aa653e628a7fc260
Felipe Francisco <156802136+fefagundes-15@users.noreply.github.com>
2024-05-28 14:05:29 -0300

    Update main.js

--- main.js
+++ main.js
@@ -37,14 +37,22 @@ function invertSign() {
 }
 
 // Dark mode
-let darkModeEnabled = false;
-
 function toggleDarkMode() {
-    darkModeEnabled = !darkModeEnabled;
-    const container = document.querySelector('body');
-    if (darkModeEnabled) {
-        container.classList.add('dark-mode');
+    const body = document.body;
+    const darkModeBtn = document.querySelector('.dark-mode-btn');
+    const calculator = document.querySelector('.calculator');
+    const button = document.querySelector('button');
+
+    // Alterar a classe do corpo para alternar entre o modo escuro e claro
+    body.classList.toggle('dark-mode');
+
+    // Verificar se a classe 'dark-mode' está presente no corpo e alterar o texto do botão
+    if (body.classList.contains('dark-mode')) {
+        darkModeBtn.textContent = 'Light Mode';
+        calculator.style.backgroundColor = '#5a5a5a';
     } else {
-        container.classList.remove('dark-mode');
+        darkModeBtn.textContent = 'Dark Mode';
+        calculator.style.backgroundColor = '#f1f1f1';
+        button.style.backgroundColor = '#e4e4e4';
     }
 }
